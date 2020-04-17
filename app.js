f0575c66f1e2be758e110c83efde3fc24746ecf9
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-04-17 20:06:34 +0300

    Initial commit, views folder inclueded

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+
+// Init App
+const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+// Home Route
+app.get('/', function (req, res) {
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'Radoslav Pavlinchov',
+            body: 'This is article one.'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'Radoslav Pavlinchov',
+            body: 'This is article two.'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Radoslav Pavlinchov',
+            body: 'This is article three.'
+        },
+    ]
+    res.render('index', {
+        title:'Hello world',
+        articles: articles
+    })
+})
+
+// Add route
+app.get('/articles/add', function(req, res) {
+    res.render('add_article', {
+        title:'Add Article'
+    })
+})
+
+// Start Server
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
+})
